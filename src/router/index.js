e036fc465724f48c0cc0ede1728d22fca1a8dfb1
ccccjing <2391166543@qq.com>
2023-05-22 21:12:55 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomePage from "../views/HomePage.vue"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/home',
+    component: HomePage
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>homepage</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+
+    }
+  },
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
